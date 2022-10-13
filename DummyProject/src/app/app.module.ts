9a307c58479fefd4bf0bc3046c96a2364b2f67fc
reetu <reetu.sood@gstv.com>
2022-10-12 23:20:21 -0400

    added code

--- DummyProject/src/app/app.module.ts
+++ DummyProject/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
